b3ba3c5a6b7dbfbdb58ba490a78ac577cbe2a966
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2018-11-29 10:42:00 -0200

    Add files via upload

---
+++ 02-Motores e Sensores/motor_passo/motor_passo.ino
@@ -0,0 +1,26 @@
+#include <Stepper.h> 
+ 
+const int stepsPerRevolution = 500; 
+  
+//Inicializa a biblioteca utilizando as portas de 8 a 11 para 
+//ligacao ao motor 
+Stepper myStepper(stepsPerRevolution, 8,10,9,11); 
+  
+void setup() 
+{ 
+ //Determina a velocidade inicial do motor 
+ myStepper.setSpeed(60);
+} 
+  
+void loop() 
+{ 
+ //Gira o motor no sentido horario a 90 graus
+ for (int i = 0; i<=3; i++)
+ {
+ myStepper.step(-512); 
+ delay(200);
+ }
+  
+ 
+ delay(200); 
+}
---
+++ 02-Motores e Sensores/motor_servo/motor_servo.ino
@@ -0,0 +1,64 @@
+#include <Servo.h>
+ 
+#define SERVO 6 // Porta Digital 6 PWM[
+
+enum Color { VERMELHO, MARROM, AZUL, AMARELO,  LARANJA, VERDE };
+ 
+Servo s; // Variável Servo
+int pos; // Posição Servo
+
+Color cor = MARROM; //com sensor
+ 
+void setup ()
+{
+  s.attach(SERVO);
+  Serial.begin(9600);
+  s.write(0); // Inicia motor posição zero
+}
+ //170 -> marrom
+ //140 -> azul
+ //110 -> laranja
+ //80 -> amarelo
+ //50 -> vermelho
+ //20 -> verde
+void loop()
+{
+  switch(cor)
+  {
+    case MARROM:
+    
+      s.write(170);
+      delay(1000);
+      break;
+    
+    case AZUL:
+    
+      s.write(140);
+      delay(1000);
+      break;
+
+    case LARANJA;
+
+      s.write(110);
+      delay(1000);
+      break;
+
+    case AMARELO;
+
+      s.write(80);
+      delay(1000);
+      break;
+
+    case VERMELHO;
+
+      s.write(50);
+      delay(1000);
+      break;
+
+    case VERDE;
+
+      s.write(20);
+      delay(1000);
+      break;
+  }
+}
